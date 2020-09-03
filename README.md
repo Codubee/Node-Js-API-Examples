c928d6df1b7bb3570611bb1c29aaa1ef1bc8c96b
Miguel <info@codubee.com>
2020-09-02 21:01:16 -0500

    Added mysql connection information:

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 A guide on how to build an API using Node.js
+In order for the connection to work you must 
+create a .env file and have the following variables
+```
+DB_HOST=AWS_RDS_URL
+DB_USER=USERNAME
+DB_PASS=PASSWORD
+```
--- hello.js
+++ hello.js
@@ -1,44 +1,38 @@
 const express = require('express')
 const app = express()
-const axios = require('axios');
+const mysql = require('mysql');
+require('dotenv').config()
+
+const con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+});
 
 //This endpoint has no route parameters
-app.get('/getEndpoint', function (req, res) {
-
-    axios.get('https://api.agify.io?name=Mike')
-    .then(function (response) {
-        // handle success and send back a 200 response with the data
-        console.log(response.data);
-        res.status(200).json(response.data);
-    })
-    .catch(function (error) {
-        // It is good practice to handle the error and send back the 
-        // error message that was received from the API call
-        console.log(error)
-        res.status(400).json({error:"An error occurred"});
-    })
-})
+app.get('/queryDb', function (req, res) {
 
 
-//This endpoint has the route parameter name
-app.get('/getEndpoint/:name', function (req, res) {
+    //Create the connection
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
 
-    //Log the request parameters
-    console.log(req.params.name)
-    let name = req.params.name;
+        // xxx is the name of our database. Have to tell mysql which database to use
+        con.query('Use xxx;');
 
-    axios.get('https://api.agify.io?name='+name)
-    .then(function (response) {
-        // handle success and send back a 200 response with the data
-        console.log(response.data);
-        res.status(200).json(response.data);
-    })
-    .catch(function (error) {
-        // It is good practice to handle the error and send back the 
-        // error message that was received from the API call
-        console.log(error)
-        res.status(400).json({error:"An error occurred"});
-    })
+        // The name of our table is Zipcodes
+        // This gets all rows from our table
+        con.query('Select * from ZipCodes;', function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+
+        //Send back the result to the front end
+
+        con.end();
+    });
+
 })
 
 app.listen(3000, () => console.log(`Example app listening at http://localhost:3000`))
