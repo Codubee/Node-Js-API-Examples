b78f918a6ce83651223a087ba56514b07ce100af
Miguel <info@codubee.com>
2020-11-22 23:04:09 -0600

    Added more examples

--- hello.js
+++ hello.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const axios = require('axios');
+//For documentation on axios refer to the npm module
+//https://www.npmjs.com/package/axios
+
 
 //This endpoint has no route parameters
 app.get('/getEndpoint', function (req, res) {
@@ -28,6 +31,51 @@ app.get('/getEndpoint/:name', function (req, res) {
     let name = req.params.name;
 
     axios.get('https://api.agify.io?name='+name)
+    .then(function (response) {
+        // handle success and send back a 200 response with the data
+        console.log(response.data);
+        res.status(200).json(response.data);
+    })
+    .catch(function (error) {
+        // It is good practice to handle the error and send back the 
+        // error message that was received from the API call
+        console.log(error)
+        res.status(400).json({error:"An error occurred"});
+    })
+})
+
+//This endpoint has a key in the headers
+app.get('/usesKey', function (req, res) {
+
+    //The key is placed inside of the dotenv file
+    //We are simple referecing it here
+    const key = process.env.API_KEY
+    
+    axios.get('https://api.thecatapi.com/v1/images/search',{
+        headers:{'Authorization':key}})
+    .then(function (response) {
+        // handle success and send back a 200 response with the data
+        console.log(response.data);
+        res.status(200).json(response.data);
+    })
+    .catch(function (error) {
+        // It is good practice to handle the error and send back the 
+        // error message that was received from the API call
+        console.log(error)
+        res.status(400).json({error:"An error occurred"});
+    })
+})
+
+//This endpoint has a body as a part of the API call
+app.post('/usesKey', function (req, res) {
+
+    
+    const body = {
+        firstName: 'Foo',
+        lastName: 'Bar'
+    }
+
+    axios.post('https://www.someplace.com',body)
     .then(function (response) {
         // handle success and send back a 200 response with the data
         console.log(response.data);
