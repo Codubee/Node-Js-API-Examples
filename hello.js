6d115369813ea782ec6979daca4d8c735cd33cf9
Miguel <info@codubee.com>
2020-11-25 12:11:33 -0600

    Added post request example

--- hello.js
+++ hello.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+/*
+    For documentation on axios refer to the npm module
+    https://www.npmjs.com/package/axios
+*/
 const axios = require('axios');
-//For documentation on axios refer to the npm module
-//https://www.npmjs.com/package/axios
+
+/*
+    If we are reading request post data, we must include this
+    in order for us to be able to read the data
+*/
+app.use(express.json());
 
 
-//This endpoint has no route parameters
+// This endpoint has no route parameters
 app.get('/getEndpoint', function (req, res) {
 
     axios.get('https://api.agify.io?name=Mike')
@@ -15,18 +23,20 @@ app.get('/getEndpoint', function (req, res) {
         res.status(200).json(response.data);
     })
     .catch(function (error) {
-        // It is good practice to handle the error and send back the 
-        // error message that was received from the API call
+        /* 
+            It is good practice to handle the error and send back the 
+            error message that was received from the API call
+        */ 
         console.log(error)
         res.status(400).json({error:"An error occurred"});
     })
 })
 
 
-//This endpoint has the route parameter name
+// This endpoint has the route parameter name
 app.get('/getEndpoint/:name', function (req, res) {
 
-    //Log the request parameters
+    // Log the request parameters
     console.log(req.params.name)
     let name = req.params.name;
 
@@ -37,18 +47,22 @@ app.get('/getEndpoint/:name', function (req, res) {
         res.status(200).json(response.data);
     })
     .catch(function (error) {
-        // It is good practice to handle the error and send back the 
-        // error message that was received from the API call
+        /* 
+            It is good practice to handle the error and send back the 
+            error message that was received from the API call
+        */ 
         console.log(error)
         res.status(400).json({error:"An error occurred"});
     })
 })
 
-//This endpoint has a key in the headers
+// This endpoint has a key in the headers
 app.get('/usesKey', function (req, res) {
 
-    //The key is placed inside of the dotenv file
-    //We are simple referecing it here
+    /*
+        The key is placed inside of the dotenv file
+        We are simple referecing it here 
+    */
     const key = process.env.API_KEY
     
     axios.get('https://api.thecatapi.com/v1/images/search',{
@@ -59,23 +73,35 @@ app.get('/usesKey', function (req, res) {
         res.status(200).json(response.data);
     })
     .catch(function (error) {
-        // It is good practice to handle the error and send back the 
-        // error message that was received from the API call
+        /* 
+            It is good practice to handle the error and send back the
+            error message that was received from the API call
+        */ 
         console.log(error)
         res.status(400).json({error:"An error occurred"});
     })
 })
 
-//This endpoint has a body as a part of the API call
-app.post('/usesKey', function (req, res) {
+// This endpoint has a body as a part of the API call
+app.post('/hasBody', function (req, res) {
 
+
+    /*
+        View the data that is coming in as part of the request
+        Data coming in should be a JSON object that has a name and an address
+        { 
+            'name':someones name,
+            'address':someones address
+        }
+    */ 
+    console.log(req.body);
     
     const body = {
-        firstName: 'Foo',
-        lastName: 'Bar'
+        name: req.body.name,
+        address: req.body.address
     }
 
-    axios.post('https://www.someplace.com',body)
+    axios.post('https://java-sample-api-2020.herokuapp.com/addPerson',body)
     .then(function (response) {
         // handle success and send back a 200 response with the data
         console.log(response.data);
